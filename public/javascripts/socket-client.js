8e76b1311e06eb08c6e58bdc287db4247070abf8
trnl <trnl.me@gmail.com>
2012-11-10 16:08:46 +0300

    Broadcasting & Buttons

--- public/javascripts/socket-client.js
+++ public/javascripts/socket-client.js
@@ -22,6 +22,18 @@
     socket.on('press',function(data){
       console.log('Pressed data: ' + data);
     });
+    
+    $('.button').each(function(){          
+    var _this = $(this);
+     $(this).prepend("<img src=\""+'http://www.gravatar.com/avatar/' + $.md5(_this.attr('data-id'))+'.jpg?size=100'+"\">"+"</img>");
+    });
+         
+    //$(document).keydown(function(code) {
+     //   console.log('Handler for .keydown() called.' + code);
+    //});
+    $('.controls').click(function(){
+        socket.emit('press', {soundid:'123'});
+    });    
   });
 })();
 
